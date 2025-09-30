8bba79e553d5f8ed96f076bbc91fde4cf4bc3302
Paula Rosa González Páez <paugonpae@alum.us.es>
2025-09-30 09:38:14 +0000

    commit

---
+++ app/modules/notepad/models.py
@@ -0,0 +1,21 @@
+from app import db
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_required, current_user
+
+from app.modules.notepad.forms import NotepadForm
+from app.modules.notepad import notepad_bp
+from app.modules.notepad.services import NotepadService
+
+notepad_service = NotepadService()
+
+class Notepad(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    user = db.relationship('User', backref='notepads', lazy=True)
+
+    def __repr__(self):
+        return f'Notepad<{self.id}, Title={self.title}, Author={self.user.username}>'
+
